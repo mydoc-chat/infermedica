1fe6035e57ebf339e14dbf9b903a1aefb0f03468
Tornike Razmadze <torniker@gmail.com>
2017-08-09 14:39:53 +0400

    updated diagnosis response struct

--- diagnosis.go
+++ diagnosis.go
@@ -2,44 +2,86 @@ package infermedica
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/pkg/errors"
 )
 
+// DiagnosisReq is a struct to request diagnosis
 type DiagnosisReq struct {
 	Sex       Sex        `json:"sex"`
 	Age       int        `json:"age"`
 	Evidences []Evidence `json:"evidence"`
 }
 
+// DiagnosisRes is a response struct for diagnosis
 type DiagnosisRes struct {
 	Question   Question                `json:"question"`
 	Conditions []DiagnosisConditionRes `json:"conditions"`
 }
 
+// QuestionType is a list of question types
+type QuestionType string
+
+const (
+	QuestionTypeSingle        QuestionType = "single"
+	QuestionTypeGroupSingle   QuestionType = "group_single"
+	QuestionTypeGroupMultiple QuestionType = "group_multiple"
+)
+
+func (qt QuestionType) Ptr() *QuestionType { return &qt }
+func (qt QuestionType) String() string     { return string(qt) }
+
+func (qt *QuestionType) IsValid() bool {
+	_, err := QuestionTypeFromString(qt.String())
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func QuestionTypeFromString(x string) (QuestionType, error) {
+	switch strings.ToLower(x) {
+	case "single":
+		return QuestionTypeSingle, nil
+	case "group_single":
+		return QuestionTypeGroupSingle, nil
+	case "group_multiple":
+		return QuestionTypeGroupMultiple, nil
+	default:
+		return "", fmt.Errorf("Unexpected value for Question Type: %q", x)
+	}
+}
+
+// Question struct
 type Question struct {
-	Type  string         `json:"type"`
+	Type  QuestionType   `json:"type"`
 	Text  string         `json:"text"`
 	Items []QuestionItem `json:"items"`
 }
 
+// QuestionItem question item struct
 type QuestionItem struct {
 	ID      string               `json:"id"`
 	Name    string               `json:"name"`
 	Choices []QuestionItemChoice `json:"choices"`
 }
 
+// QuestionItemChoice question item choice struct
 type QuestionItemChoice struct {
-	ID    string `json:"id"`
-	Label string `json:"label"`
+	ID    EvidenceChoiceID `json:"id"`
+	Label string           `json:"label"`
 }
 
+// DiagnosisConditionRes is a response struct for condition + probability
 type DiagnosisConditionRes struct {
 	Condition
 	Probability float64 `json:"probability"`
 }
 
+// Diagnosis is a func to request diagnosis for given data
 func (a *App) Diagnosis(dr DiagnosisReq) (*DiagnosisRes, error) {
 	if !dr.Sex.IsValid() {
 		return nil, errors.New("Unexpected value for Sex")
