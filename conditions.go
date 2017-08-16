20a5079139b6d8150be8ac4d3936656dec39967f
Tornike Razmadze <torniker@gmail.com>
2017-08-16 15:02:51 +0400

    added prevalence, acuteness and severity types

--- conditions.go
+++ conditions.go
@@ -2,8 +2,112 @@ package infermedica
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strings"
 )
+
+type Prevalence string
+
+const (
+	PrevalenceVeryRare Prevalence = "very_rare"
+	PrevalenceRare     Prevalence = "rare"
+	PrevalenceModerate Prevalence = "moderate"
+	PrevalenceCommon   Prevalence = "common"
+)
+
+func (p Prevalence) Ptr() *Prevalence { return &p }
+func (p Prevalence) String() string   { return string(p) }
+
+func (p *Prevalence) IsValid() bool {
+	_, err := PrevalenceFromString(p.String())
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func PrevalenceFromString(x string) (Prevalence, error) {
+	switch strings.ToLower(x) {
+	case "very_rare":
+		return PrevalenceVeryRare, nil
+	case "rare":
+		return PrevalenceRare, nil
+	case "moderate":
+		return PrevalenceModerate, nil
+	case "common":
+		return PrevalenceCommon, nil
+	default:
+		return "", fmt.Errorf("Unexpected value for Prevalence: %q", x)
+	}
+}
+
+type Acuteness string
+
+const (
+	AcutenessChronic                  Acuteness = "chronic"
+	AcutenessChronicWithExacerbations Acuteness = "chronic_with_exacerbations"
+	AcutenessAcutePotentiallyChronic  Acuteness = "acute_potentially_chronic"
+	AcutenessAcute                    Acuteness = "acute"
+)
+
+func (a Acuteness) Ptr() *Acuteness { return &a }
+func (a Acuteness) String() string  { return string(a) }
+
+func (a *Acuteness) IsValid() bool {
+	_, err := AcutenessFromString(a.String())
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func AcutenessFromString(x string) (Acuteness, error) {
+	switch strings.ToLower(x) {
+	case "chronic":
+		return AcutenessChronic, nil
+	case "chronic_with_exacerbations":
+		return AcutenessChronicWithExacerbations, nil
+	case "acute_potentially_chronic":
+		return AcutenessAcutePotentiallyChronic, nil
+	case "acute":
+		return AcutenessAcute, nil
+	default:
+		return "", fmt.Errorf("Unexpected value for Acuteness: %q", x)
+	}
+}
+
+type Severity string
+
+const (
+	SeverityMild     Severity = "mild"
+	SeverityModerate Severity = "moderate"
+	SeveritySevere   Severity = "severe"
+)
+
+func (s Severity) Ptr() *Severity { return &s }
+func (s Severity) String() string { return string(s) }
+
+func (s *Severity) IsValid() bool {
+	_, err := SeverityFromString(s.String())
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func SeverityFromString(x string) (Severity, error) {
+	switch strings.ToLower(x) {
+	case "mild":
+		return SeverityMild, nil
+	case "moderate":
+		return SeverityModerate, nil
+	case "severe":
+		return SeveritySevere, nil
+	default:
+		return "", fmt.Errorf("Unexpected value for Severity: %q", x)
+	}
+}
 
 type Condition struct {
 	ID         string `json:"id"`
@@ -15,9 +119,9 @@ type ConditionRes struct {
 	Condition
 	SexFilter   string          `json:"sex_filter"`
 	Categories  []string        `json:"categories"`
-	Prevalence  string          `json:"prevalence"`
-	Acuteness   string          `json:"acuteness"`
-	Severity    string          `json:"severity"`
+	Prevalence  Prevalence      `json:"prevalence"`
+	Acuteness   Acuteness       `json:"acuteness"`
+	Severity    Severity        `json:"severity"`
 	Extras      ConditionExtras `json:"extras"`
 	TriageLevel string          `json:"triage_level"`
 }
