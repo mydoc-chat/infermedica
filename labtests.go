992780292fd2741164003ae79ba0bd2b22fa2944
Tornike Razmadze <torniker@gmail.com>
2017-10-31 18:01:42 +0400

    added lab tests recommend endpoint handler

--- labtests.go
+++ labtests.go
@@ -3,6 +3,8 @@ package infermedica
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/pkg/errors"
 )
 
 type LabTestsRes struct {
@@ -47,6 +49,42 @@ func (a *App) LabTestByID(id string) (*LabTestsRes, error) {
 		return nil, err
 	}
 	r := LabTestsRes{}
+	err = json.NewDecoder(res.Body).Decode(&r)
+	if err != nil {
+		return nil, err
+	}
+	return &r, nil
+}
+
+type LabTestsRecommendRes struct {
+	Recommended *[]LabTestsRecommendation `json:"recommended"`
+	Obligatory  *[]LabTestsRecommendation `json:"obligatory"`
+}
+type LabTestsRecommendation struct {
+	PanelID  string       `json:"panel_id"`
+	Name     string       `json:"name"`
+	Position int          `json:"position"`
+	LabTests []LabTestsID `json:"lab_tests"`
+}
+type LabTestsID struct {
+	ID string `json:"id"`
+}
+
+// Recommend is a func to request lab test recommendations for given data
+func (a *App) Recommend(dr DiagnosisReq) (*LabTestsRecommendRes, error) {
+	if !dr.Sex.IsValid() {
+		return nil, errors.New("Unexpected value for Sex")
+	}
+	req, err := a.prepareRequest("POST", "lab_tests/recommend", dr)
+	if err != nil {
+		return nil, err
+	}
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	r := LabTestsRecommendRes{}
 	err = json.NewDecoder(res.Body).Decode(&r)
 	if err != nil {
 		return nil, err
